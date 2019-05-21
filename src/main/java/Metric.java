46895a33faae3bd8a8f4c17ce8a38e51cbf94e65
farid <34004467@parisnanterre.fr>
2019-05-21 15:59:44 +0200

    return real name of metric

--- src/main/java/Metric.java
+++ src/main/java/Metric.java
@@ -15,10 +15,47 @@ public class Metric {
     public Metric() {
     }
 
+    public Metric(Map<String, String> m) {
+        this.m = m;
+    }
+
+    private String askedMetrics = "bugs,confirmed_issues,code_smells,sqale_index,coverage,duplicated_lines_density,duplicated_blocks";
+
+    public String getAskedMetrics() {
+        return askedMetrics;
+    }
+    private Map<String,String> m=new HashMap<>();
+    void addmetric(Map m){
+        m.put("nombre de bug","bugs");
+        m.put("vulnérabilités","confirmed_issues");
+        m.put("code smells","code smells");
+        m.put("debt","sqale_index");
+        m.put("coverage","coverage");
+        m.put("duplications","duplicated_lines_density");
+        m.put("duplicated blocks","duplicated_blocks");
+
+    }
+
+    public static String getRealName(Map m,String name)
+    {
+        for(Object s:m.keySet()){
+            if(m.get(s).equals(name)){
+                return s.toString();
+            }
+            else{
+                return "Error";
+            }
+        }
+        return null;
+    }
+
+
+
+
     public Map<String, Double> getMetrics(String compName){
-        String askedMetrics = "bugs,confirmed_issues,code_smells,sqale_index,coverage,duplicated_lines_density,duplicated_blocks";
+
         try {
-            URL url = new URL("http://localhost:9000/api/measures/component?metricKeys=" + askedMetrics + "&component=" + compName + "");
+            URL url = new URL("http://localhost:9000/api/measures/component?metricKeys=" + getAskedMetrics() + "&component=" + compName + "");
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("GET");
             conn.setRequestProperty("hello", "application/json");
@@ -38,7 +75,7 @@ public class Metric {
             for(JsonElement metric : metrics) {
                 name = metric.getAsJsonObject().get("metric").toString();
                 value = metric.getAsJsonObject().get("value").getAsDouble();
-                map.put(name, value);
+                map.put(getRealName(this.m,name), value);
             }
             System.out.println(map);
             conn.disconnect();
